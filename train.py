fa52e4d5d45c56b50ef290b6865f24a5f6706c29
YurinoHosokawa <yu.uy.zz.ao.2@gmail.com>
2023-03-28 07:08:36 +0900

    Fix : roop method Add :  output pickle file

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from common.functions import *
 from common.gradient import numerical_gradient
 from dataset import Dataset
 from tqdm import  tqdm
+import pickle
 
 # dataset
 ds = Dataset()
@@ -20,12 +21,16 @@ nw = TwoLayerNet(input_size, hidden_size, output_size)
 
 h = 1e-4
 for j in tqdm(range(100)):
+    grad = {}
+    # print('j=',j)
     for key in ('W1', 'b1', 'W2', 'b2'):
-        grad = np.zeros_like(nw.params[key])
-        it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+        # print('key=',key)
+        _grad = np.zeros_like(nw.params[key])
+        it = np.nditer(nw.params[key], flags=['multi_index'], op_flags=['readwrite'])
         while not it.finished:
             idx = it.multi_index
-        # for i in range(len(grad)):
+            #print('idx=',idx)
+            # for i in range(len(grad)):
             # grad[0][0]
             tmp = nw.params[key][idx]
 
@@ -37,9 +42,17 @@ for j in tqdm(range(100)):
             y = nw.predict(x)
             fxh2 = cross_entropy_error(y, t)
 
-            grad[idx] = (fxh1 - fxh2) / (2 * h)
-            nw.params['W1'][idx] = tmp
+            _grad[idx] = (fxh1 - fxh2) / (2 * h)
+            nw.params[key][idx] = tmp
+            it.iternext()   
+        grad[key] = _grad
     for key in ('W1', 'b1', 'W2', 'b2'):
             nw.params[key] -= 1 * grad[key]
     
 print(grad)
+
+print('Creating model ...', end='')
+pkl_file  = './model.pkl'
+with open(pkl_file, 'wb') as f:
+    pickle.dump(nw.params, f, -1)
+print(' -> Done!')
